b0c70257dc689c5ad153903b0a20dec38e810a85
CrazyStormZ <earthhnapat@gmail.com>
2021-05-09 16:44:18 +0700

    Update Files

--- covidspamdetect.py
+++ covidspamdetect.py
@@ -1,4 +1,3 @@
-# https://jahchaisang.gitbooks.io/fra142-gui/content/chapter1-0.html tkinter
 import tkinter as tk
 import pickle
 import pandas as pd
@@ -6,11 +5,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 import re
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 nltk.download('stopwords')
+############################### preprocessing
 data = pd.read_csv('tweet.csv')
 X, y= data['Tweet'], data['class_label']
 documents = []
-from nltk.stem import WordNetLemmatizer
 stemmer = WordNetLemmatizer()
 for sen in range(0, len(X)):
     document = re.sub(r'\W', ' ', str(X[sen]))
@@ -33,7 +33,7 @@ X = vectorizer.fit_transform(documents).toarray()
 with open("TheModel.pkl", 'rb') as file:
     model = pickle.load(file)
 
-############################################## setup
+############################################## UI setup
 window = tk.Tk()
 window.geometry('640x480')
 window.resizable(False, False)
@@ -41,6 +41,9 @@ window.title("Spam Detector")
 text = tk.StringVar()
 entext = tk.StringVar(value='Write here!')
 text.set("")
+bg = tk.PhotoImage(file = "covid.png")
+bgp = tk.Label( window, image = bg)
+bgp.place(x = 300,y = 365)
 ############################################## function
 def printPos():
     text.set("██████╗░░█████╗░░██████╗██╗████████╗██╗██╗░░░██╗███████╗\n"+
@@ -58,6 +61,7 @@ def printNeg():
           "██║░╚███║███████╗╚██████╔╝██║░░██║░░░██║░░░██║░░╚██╔╝░░███████╗\n"+
           "╚═╝░░╚══╝╚══════╝░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░╚═╝░░░╚══════╝\n"+
           "It is a spam")        
+#Prediction
 def predict():
     inputt = e1.get("1.0", "end-1c")
     res = model.predict(vectorizer.transform([inputt]).toarray())
@@ -68,9 +72,9 @@ def predict():
 
 ########################################### UI
 w = tk.Label(window, text="Welcome to Covid Spam Detector",font=("Time New Roman", 30))
-w.place(x=5,y=0)
+w.place(relx=.5, rely=.05, anchor="center")
 w2 = tk.Label(window, text="Please input your sentence",font=("Time New Roman", 15))
-w2.place(x=180,y=50)
+w2.place(relx=.5, rely=.125, anchor="center")
 
 l1 = tk.Label(window, text=">")
 l1.place(x=40,y=85)
@@ -81,8 +85,8 @@ conf = tk.Button(window, text="Confirm", fg="red", command=predict)
 conf.place(x=275,y=180)
 
 res = tk.Label(window, text="result", textvariable=text,font=("Time New Roman", 10))
-res.place(x=30,y=250)
-
+#res.place(x=30,y=250)
+res.place(relx=.5, rely=.625, anchor="center")
 
 window.mainloop()
 
